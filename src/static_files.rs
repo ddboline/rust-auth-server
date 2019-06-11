2b8ac309fdfc6732dd68b0229fb29ea9c35b8bd7
Daniel Boline <ddboline@gmail.com>
2019-06-10 22:34:18 -0400

    ...

---
+++ src/static_files.rs
@@ -0,0 +1,32 @@
+use actix_web::{Error, HttpResponse};
+use futures::Future;
+
+pub fn index_html() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../static/index.html"))
+}
+
+pub fn main_css() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/css; charset=utf-8")
+        .body(include_str!("../static/main.css"))
+}
+
+pub fn register_html() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../static/register.html"))
+}
+
+pub fn main_js() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/javascript; charset=utf-8")
+        .body(include_str!("../static/main.js"))
+}
+
+pub fn login_html() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../static/login.html"))
+}
