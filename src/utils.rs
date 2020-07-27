763042e3df6c57ad8eddd14c281481afef00d45b
Daniel Boline <ddboline@gmail.com>
2020-07-27 13:11:57 -0400

    small refactor

--- src/utils.rs
+++ src/utils.rs
@@ -12,10 +12,10 @@ const DEFAULT_ALGORITHM: Algorithm = Algorithm::HS256;
 
 pub fn hash_password(plain: &str) -> Result<String, ServiceError> {
     // get the hashing cost from the env variable or use default
-    let hashing_cost: u32 = match env::var("HASH_ROUNDS") {
-        Ok(cost) => cost.parse().unwrap_or(DEFAULT_COST),
-        _ => DEFAULT_COST,
-    };
+    let hashing_cost = env::var("HASH_ROUNDS")
+        .ok()
+        .and_then(|c| c.parse().ok())
+        .unwrap_or(DEFAULT_COST);
     debug!("{}", &hashing_cost);
     hash(plain, hashing_cost).map_err(|_| ServiceError::InternalServerError)
 }
